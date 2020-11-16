fdda2110024366a2a0e83c83faf3d2783e4629f7
Dmitry Navitski <arbalestindigo@gmail.com>
2020-11-16 16:30:57 +0300

    Initial Commit

---
+++ RssFeedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RssFeedApp
+//
+//  Created by Dmitry on 16.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
